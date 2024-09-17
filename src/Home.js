95ef33dc4f9dc4d9d77b53b89ec76d9a239a759c
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-09-17 14:03:07 +0300

    useState hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+// state hook
+import { useState } from "react";
+
 const Home = () => {
+    // using the useState hook to create a state variable
+  const [name, changeName] = useState("mario");
+  const [age, changeAge] = useState(25);
+
+  const handleClick = (name) => {
+    changeName("Luigi");
+    changeAge(30);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={() => handleClick("mario")}>click me</button>
     </div>
   );
 };
