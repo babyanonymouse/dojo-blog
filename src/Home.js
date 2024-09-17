69346852587c835347e8e44b7ac3b4239873eac8
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-09-17 16:15:22 +0300

    conditional loading

--- src/Home.js
+++ src/Home.js
@@ -5,30 +5,30 @@ import Bloglist from "./Bloglist";
 const Home = () => {
   // using the useState hook to create a state variable
   const [blogs, changeBlogs] = useState(null);
+  //   loading pending data
+  const [pending, changePending] = useState(true);
 
   // useEffect hook to fetch data/db.json data
   useEffect(() => {
-    fetch("http://localhost:3000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        changeBlogs(data);
-      });
+    //setTimeout to simulate a delay
+    setTimeout(() => {
+      fetch("http://localhost:3000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          changeBlogs(data);
+          changePending(false); //pending data is false if data is fetched
+        });
+    }, 500);
   }, []);
 
   return (
     <div className="home">
+      {/* pending message */}
+      {pending && <div>Loading...</div>}
       {/* using props to pass data to bloglist */}
       {blogs && <Bloglist blogs={blogs} title="All Blogs!" />}
-      {/* filter Mario's blogs display */}
-      {/* {blogs && (
-        <Bloglist
-          blogs={blogs.filter((blog) => blog.author === "Mario")}
-          title={"Mario's Blogs"}
-        />
-      )} */}
-      {blogs && <Bloglist blogs={blogs} />}
     </div>
   );
 };
