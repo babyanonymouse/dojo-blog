102a9cd3e7350d15e7bc117308852c8c527f6271
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-09-17 13:28:04 +0300

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = (Math.random() * 100).toFixed(0);
-  // const person = { name: "Yoshi", age: 30 };
-  const google = "https://google.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* Arrays are not supported */}
-        {/* <p>{person}</p> */}
-
-        <p>{10}</p>
-        <p>{"Hello Ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <a href={google}>Google Link</a>
+        <Home />
       </div>
     </div>
   );
 }
-
 export default App;
