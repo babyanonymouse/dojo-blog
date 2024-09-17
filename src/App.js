4b4ccc42893a24dc4cd94037269c5dd959ea21dc
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-09-17 13:14:33 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = (Math.random() * 100).toFixed(0);
+  // const person = { name: "Yoshi", age: 30 };
+  const google = "https://google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* Arrays are not supported */}
+        {/* <p>{person}</p> */}
+
+        <p>{10}</p>
+        <p>{"Hello Ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <a href={google}>Google Link</a>
       </div>
     </div>
   );
