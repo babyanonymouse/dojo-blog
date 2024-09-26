78dbe86849cec9c7b1920edb7e756427fc9323d4
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-09-26 16:34:42 +0300

    blog editing

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    pending,
+    error,
+  } = useFetch("http://localhost:8800/blogs/" + id); //using custom hook
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {pending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written By {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
